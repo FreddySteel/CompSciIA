8928f984d83a9c02654cd40940e9e69aba0c12cc
FreddySteel <17steel_f@bexleygs.co.uk>
2023-07-19 10:14:31 +0100

    fix take order.

--- src/SettingsGUI.java
+++ src/SettingsGUI.java
@@ -17,16 +17,16 @@ public class SettingsGUI extends JPanel {
         // Add buttons to panel
         add(Box.createVerticalGlue());
         add(editCustomersButton);
-        add(Box.createVerticalStrut(10)); // Adds vertical space between buttons
+        add(Box.createVerticalStrut(100)); // Adds vertical space between buttons
         add(editInventoryButton);
-        add(Box.createVerticalStrut(10)); // Adds vertical space between buttons
+        add(Box.createVerticalStrut(100)); // Adds vertical space between buttons
         add(editInvoicesButton);
         add(Box.createVerticalGlue());
     }
 
     private JButton createButton(String text) {
         JButton button = new JButton(text);
-        button.setMaximumSize(new Dimension(200, 40)); // Sets max width and height
+        button.setMaximumSize(new Dimension(300, 500)); // Sets max width and height
         button.setAlignmentX(CENTER_ALIGNMENT); // Center alignment
         return button;
     }
